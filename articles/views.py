73b38ded4f25fc6d39d637183fc957c8eb6a4abf
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-06-30 15:43:00 +0400

    15 added fisrt object to db

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.db import models
 
 # Create your views here.
-class Article(models.Model):
-    title = models.TextField()
-    content = models.TextField()
--- trydjango/views.py
+++ trydjango/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from articles.models import Article
 
-HTML_STRING = """
-<h1>Hello world!</h1>
-"""
 
 def home_view(request):
+    obj = Article.objects.get(id=1)
+    HTML_STRING = f"""
+    <h1>Hello world!</h1>
+    <p>Title - {obj.title}</p>
+    <p>Content - {obj.content}</p>
+    <p>ID - {obj.id}</p>
+    """
     return HttpResponse(HTML_STRING)
